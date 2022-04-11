5ae8f735dbd1615ff8eb588203936b748979ebfe
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-11 12:10:45 +0300

    create case insensitive findMatching function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+// function findMatching(enter){
+//     const matching = drivers.filter(driver => driver === enter)
+//     return matching;
+// }
+// console.log(findMatching('Bobby'));
+
+
+function findMatching(arr, name){
+    return arr.filter(arr => arr.toLowerCase() === name.toLowerCase())
+}
+console.log(findMatching(drivers, 'bobby'));
